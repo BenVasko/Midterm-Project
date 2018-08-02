ec2513cca8f6f6ce806473b7e8c9353d261a677b
Ben Vasko <BenVasko@Bens-MacBook-Pro.local>
2018-08-02 11:38:02 -0400

    Started checkout page and added a little JS to cart

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 " use strict";
+$(document).ready(() => {
+    const hideItemPopups = () => {
+        $(".product-popup").toggle();
+    }
 
-const hideItemPopups = () => {
-    $(".product-popup").toggle();
-}
+    const hideCart = () => {
+        $(".slideOutCartParent").toggle();
+    }
 
-const hideCart = () => {
-    $(".slideOutCart").toggle();
-}
+    hideItemPopups();
+    hideCart();
 
-hideItemPopups();
-hideCart();
-
+    $("body").on("click", "a.cart", (e) => {
+        $(".slideOutCartParent").slideToggle(1000);
+    });
+});
