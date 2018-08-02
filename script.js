facbbb9733978383882c4ad21eb389f7213d858f
John Permaloff <jvpermaloff@gmail.com>
2018-08-02 11:37:25 -0400

    popups

--- script.js
+++ script.js
@@ -6,11 +6,59 @@ const hideItemPopups = () => {
 
 // hideItemPopups();
 
-$(".item-2").click((e) => {
-    $("#hidePopup").toggle();
-    console.log("hi");
+// Vinyl
+$("To be Determined").click((e) => {
+    $("#hidePopupVinyl").toggle();
 });
-$(".product-popup").click((e) => {
-    $("#hidePopup").toggle();
-    console.log("hi");
+$(".product-popup-vinyl").click((e) => {
+    $("#hidePopupVinyl").toggle();
+});
+// CD
+$("To be Determined").click((e) => {
+    $("#hidePopupCD").toggle();
+});
+$(".product-popup-cd").click((e) => {
+    $("#hidePopupCD").toggle();
+});
+// burgundy shirt
+$(".item-3").click((e) => {
+    $("#hidePopupBurgundy").toggle();
+});
+$(".product-popup-burgundy").click((e) => {
+    $("#hidePopupBurgundy").toggle();
+});
+// black shirt
+$(".item-2").click((e) => {
+    $("#hidePopupBlack").toggle();
+});
+$(".product-popup-black").click((e) => {
+    $("#hidePopupBlack").toggle();
+});
+// White Shirt
+$("To be Determined").click((e) => {
+    $("#hidePopupWhite").toggle();
+});
+$(".product-popup-white").click((e) => {
+    $("#hidePopupWhite").toggle();
+});
+// Dad Hat
+$("To be Determined").click((e) => {
+    $("#hidePopupHat").toggle();
+});
+$(".product-popup-hat").click((e) => {
+    $("#hidePopupHat").toggle();
+});
+// Poster #1
+$("To be Determined").click((e) => {
+    $("#hidePopupP1").toggle();
+});
+$(".product-popup-p1").click((e) => {
+    $("#hidePopupP1").toggle();
+});
+// Poster #2
+$("To be Determined").click((e) => {
+    $("#hidePopupP2").toggle();
+});
+$(".product-popup-p1").click((e) => {
+    $("#hidePopupP2").toggle();
 });
